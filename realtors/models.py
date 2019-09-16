814d5e2c640c0991008b8ea7d7ec94a3dbb327d4
mattdeba <debarge.matthieu@gmail.com>
2019-09-16 10:21:32 +0200

    models creation

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    is_mvp = models.BooleanField(default=False)
+    phone = models.CharField(max_length=50)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    
